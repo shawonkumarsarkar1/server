cb0fd93b41ea35db651ec0616744e026ec4c86a0
shawonkumarsarkar1 <shawonkumarsarkar1@gmail.com>
2025-11-18 22:49:25 +0600

    Updated the sendSuccessResponse function to sendResponse function

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,34 @@
+import { Response } from 'express';
+
+interface IMeta {
+  limit: number;
+  page: number;
+  total: number;
+  totalPage: number;
+}
+
+interface IResponse<T> {
+  statusCode: number;
+  message?: string;
+  data: T;
+  meta?: IMeta;
+}
+
+const sendResponse = <T>(res: Response, resData: IResponse<T>): void => {
+  const {
+    statusCode = 200,
+    message = 'Request completed successfully',
+    data = [],
+    meta,
+  } = resData;
+  const response = {
+    status: 'success',
+    message,
+    data,
+    meta,
+  };
+
+  res.status(statusCode).json(response);
+};
+
+export default sendResponse;
