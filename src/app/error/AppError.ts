d985f1b963e3f330f3d7d4d432ceb8fd6a1b4713
shawonkumarsarkar1 <shawonkumarsarkar1@gmail.com>
2025-11-23 23:33:05 +0600

    Added app error to throw error message with status code

---
+++ src/app/error/AppError.ts
@@ -0,0 +1,11 @@
+class AppError extends Error {
+  public statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    Object.setPrototypeOf(this, AppError.prototype);
+  }
+}
+
+export default AppError;
